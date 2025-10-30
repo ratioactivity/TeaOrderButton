d8c667ea012d7589b39de9f1a3bb09a38196d686
ratioactivity <semmatron@gmail.com>
2025-10-29 19:55:17 -0500

    Refine button label styling

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const button = document.getElementById('order-btn');
 const sound = document.getElementById('click-sound');
+const orderFormUrl = 'https://butter-look-617.notion.site/29b86c3bb6b980c6a41ec40c43c31303';
 
 button.addEventListener('click', () => {
   sound.currentTime = 0;
-  sound.play();
-  window.open('https://butter-look-617.notion.site/29b86c3bb6b980c6a41ec40c43c31303', '_blank');
+
+  const playPromise = sound.play();
+  if (playPromise && typeof playPromise.catch === 'function') {
+    playPromise.catch(() => {
+      /* Some browsers block autoplay; safely ignore errors */
+    });
+  }
+
+  window.open(orderFormUrl, '_blank', 'noopener');
 });
