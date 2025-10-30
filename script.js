03d0a24b7dd95b77fd7aade43c8b4589a263fd5e
ratioactivity <semmatron@gmail.com>
2025-10-29 19:32:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const button = document.getElementById('order-btn');
 const sound = document.getElementById('click-sound');
 
 button.addEventListener('click', () => {
-  // play shimmer
   sound.currentTime = 0;
   sound.play();
-
-  // open Notion order form
   window.open('https://butter-look-617.notion.site/29b86c3bb6b980c6a41ec40c43c31303', '_blank');
 });
